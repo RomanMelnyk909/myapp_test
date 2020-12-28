244a122f72a7e2ae44d1936cf424ce4b40d8a7fc
RomanMelnyk909 <45914004+RomanMelnyk909@users.noreply.github.com>
2020-12-28 20:12:34 +0200

    JSX

--- src/app/App.js
+++ src/app/App.js
@@ -1,70 +1,54 @@
 
 import React from 'react';
 import './App.css';
-import Header from '../header/Header';
-import Main from '../main/Main';
-import Footer from '../footer/Footer';
 
-let nav = {
-  main : '/index',
-  about : '/about',
-  price : '/buy/price',
-  hello : 'Hello'
-}
 
 class App extends React.Component{
   constructor(props) {
     super(props);
     this.state = { 
-      text : 'Hello',
-      text2 : 555,
-      text3 : 'Alloha'
+      go: 'gooooo'
     }
-    this.showText = this.showText.bind(this);
-    this.showText2 = this.showText2.bind(this);
-    this.move = this.move.bind(this);
+    
+  }
+  test() { 
+    return 111;
   }
   
-  showText() { 
-    console.log('work');
-    this.setState({text: 'hi'})
-  }
-
-  showText2(e) { 
-   
-    this.setState({text2 : e.target.value})
-    console.log(e.target.value)
-  }
-
-  move(e) { 
-    console.log(e.target.textContent);
-    this.setState({text3 : 'ohohoh'})
-  }
-
 render() {
-  
+
+  let a = 44;
+  let b = {
+    name: 'ivan',
+    lastname: 'ivanov'
+  };
+  let c = ['hello', 'ivanov', 'ivan', 'ivanovich'];
+
   return (
-    <div>
-      <button onClick = {this.showText}>Push1</button>
-      <input defaultValue={this.state.text} onInput={this.showText2}/>
-      <div onMouseMove = {this.move}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam nemo at dolor fuga quibusdam non incidunt voluptas accusantium iste autem voluptatum totam rerum perferendis aspernatur officiis, odit repellat, eum culpa?
-      </div>
-      <p>{this.state.text}</p>
-      <p>{this.state.text2}</p>
-      <p>{this.state.text3}</p>
+    <div className='ty'>
+      <h1>{this.state.go}</h1>
+      <h2>{3 + 2}</h2>
+      <ul>
+        {c.map((elem, index) => {
+          return <li key={elem}>{index+1} {elem}</li>
+        })}
+        {/* {c.map(function(elem) {
+          return <li>{elem}</li>
+          
+        })} */}
+      </ul>
+      <div>{Math.random()}</div>
+      <div>{this.test()}</div>
+      <div>{a}</div>
+      <div {...b}></div>
+
+      <hr/>
+      <p>{false}</p>
+      <hr/>
+      <p>&#38;</p>
+      <input defaultValue = 'hello' />
+
     </div>
-
-
-
-
-
-
-    // <div className="App">
-    //   <Header title='New site' hero='GOO gogo' nav={nav}/>
-    //   {/* <Main  /> */}
-    //   {/* <Footer /> */}
-    // </div>
   );
 }
 }
